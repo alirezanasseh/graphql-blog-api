7e98d349d8945ebc8fc6ee4ba78928aac00eaf03
Alireza <alireza.nasseh@gmail.com>
2021-12-12 21:17:28 +0330

    Removed mongoose

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,11 +1,9 @@
 import {Application} from 'express';
 import expressLoader from './express.loader';
-import mongooseLoader from './mongoose.loader';
 
 export default async ({expressApp}: {expressApp: Application}) => {
     try {
         const server = await expressLoader({app: expressApp});
-        await mongooseLoader();
 
         return server;
     } catch (e) {
